cd8e6399811c41f94f99179f6d57bf3872cdb2a9
Secuii <38568884+Secuii@users.noreply.github.com>
2020-05-24 16:26:09 +0200

    Add files via upload

---
+++ ButtonSc.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSc : MonoBehaviour
+{
+    // the button will be anchored to a pivot for the player to get a reference to move it after
+    public GameObject pivot;
+    public int pivotValue;
+}
